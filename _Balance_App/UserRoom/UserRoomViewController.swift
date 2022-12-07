bc04b455a31be74268d77ef7d74308eede0231a8
TomasVNoctrus <Tmas.css@gmail.com>
2022-12-07 07:19:56 +0200

    working navigation

--- _Balance_App/UserRoom/UserRoomViewController.swift
+++ _Balance_App/UserRoom/UserRoomViewController.swift
@@ -15,8 +15,10 @@ class UserRoomViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
+        
     }
 
 
